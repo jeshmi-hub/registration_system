210a3fd19ea66d75e63cefc18f5772eb546c20b0
jeshmi-hub <rajakjeshmi19@gmail.com>
2023-07-29 20:48:39 +0545

    MySQL database connection successful

--- form.php
+++ form.php
@@ -1,4 +1,30 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$myDB = "tutorial";
+
+$conn = new mysqli($servername, $username, $password,$myDB);
+
+if($conn->connect_error){
+    die("Connection failed: ". $conn->connect_error);
+}else{
+    $sql = "SELECT * FROM student";
+    $result = $conn ->query($sql);
+    if($result->num_rows>0){
+       while($row = $result->fetch_assoc()){
+        echo "id: " . $row["id"]."firstName: ". $row["firstName"]. "lastName: ". $row["lastName"]. "<br>";
+       }
+    }else{
+       echo "Failed to delete the record";
+    }
+}
+
+
+
+$conn -> close();
+
+
 
 
 ?>
